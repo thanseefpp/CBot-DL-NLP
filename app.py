4d04e7ce90274bd175186cbce9376793a2472930
Thanseef <androtsf@gmail.com>
2023-01-23 15:48:15 +0530

    file extracting test completed

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
 import tensorflow as tf
+from model.chatbot_database import create_table,find_parent
+from datetime import datetime
+import json
 
 
+#file path of the dataset
+file_path = "Dataset/RC_2015-01"
 
-"""
-    Extract command from user input and Test with trained model
-"""
+
+def format_data(data):
+    # This will format the data contain (\n,\r,single quote - double quote)
+    data = data.replace("\n", " newlinechar ").replace("\r", " newlinechar ").replace('"', "'")
+    return data
+
+
+if __name__ == "__main__":
+    # This will create a sql lite table.
+    create_table()
+    row_counter = 0
+    paired_rows = 0
+    
+    # Here this will open the file and extract the data.
+    with open(file_path,buffering=1000) as f:
+        for row in f:
+            row_counter += 1
+            row = json.loads(row)
+            parent_id = row['parent_id']
+            body = format_data(row['body'])
+            created_utc = row['created_utc']
+            score = row['score']
+            subreddit = row['subreddit']
+            parent_data = find_parent(parent_id)
+            # print(parent_data)
--- model/chatbot_database.py
+++ model/chatbot_database.py
@@ -1,6 +1,4 @@
-import json
 import sqlite3
-from datetime import datetime
 
 current_year_month = "2023-01"
 
@@ -8,9 +6,6 @@ sql_transaction = []
 
 connection = sqlite3.connect(f'{current_year_month}.db')
 c = connection.cursor()
-
-#file path of the dataset
-file_path = ""
 
 def create_table():
     c.execute(
@@ -20,20 +15,21 @@ def create_table():
             comment_id TEXT UNIQUE,
             parent TEXT,
             comment TEXT,
-            subreddit TEXT
+            subreddit TEXT,
             unix INT,
             score INT
         )
       """
     )
-    
-
-if __name__ == "__main__":
-    # This will create a sql lite table.
-    create_table()
-    row_counter = 0
-    paired_rows = 0
-    
-    # with open(file_path)
-    
-
+  
+def find_parent(id):
+    try:
+        query = f"SELECT comment FROM chat_reply WHERE comment_id = {id} LIMIT 1"
+        c.execute(query)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return False
+    except Exception as e:
+        return False
