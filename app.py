1db0b198f99f47a3d418a209526d3ff1f31c6094
Thanseef <androtsf@gmail.com>
2023-01-23 22:03:59 +0530

    inserted bulk 1m datas to database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-from model.chatbot_database import create_table,find_parent
+from model.chatbot_database import create_table,find_parent,find_existing_score,\
+    sql_insert_replace_comment,sql_insert_has_parent,sql_insert_no_parent
 from datetime import datetime
 import json
 
@@ -13,6 +14,19 @@ def format_data(data):
     data = data.replace("\n", " newlinechar ").replace("\r", " newlinechar ").replace('"', "'")
     return data
 
+def acceptable_data(data):
+    if len(data.split(' ')) > 50 or len(data) < 1:
+        return False
+    elif len(data) > 1000:
+        return False
+    elif data == '[deleted]' or data == '[removed]':
+        return False
+    else:
+        return True
+    
+# def singleInsert():
+#     c.execute("""INSERT INTO chat_reply SET(t3_2qx5au,t1_cnaudqw,lorlipone,KotakuInAction,1420075348,2)""")
+
 
 if __name__ == "__main__":
     # This will create a sql lite table.
@@ -21,8 +35,12 @@ if __name__ == "__main__":
     paired_rows = 0
     
     # Here this will open the file and extract the data.
-    with open(file_path,buffering=1000) as f:
+    with open(file_path,buffering=2000) as f:
         for row in f:
+            # print(row)
+            # if row_counter >= 5:
+            #     break
+            # row_counter += 1
             row_counter += 1
             row = json.loads(row)
             parent_id = row['parent_id']
@@ -30,5 +48,33 @@ if __name__ == "__main__":
             created_utc = row['created_utc']
             score = row['score']
             subreddit = row['subreddit']
+            comment_id = row['name']
+            author = row['author']
             parent_data = find_parent(parent_id)
-            # print(parent_data)
+            if score >= 2:
+                if acceptable_data(body):
+                    existing_comment_score = find_existing_score(parent_id)
+                    if existing_comment_score:
+                        if score > existing_comment_score:
+                            sql_insert_replace_comment(parent_id=parent_id,comment_id=comment_id,
+                                                       author=author,parent_data=parent_data,subreddit=subreddit,
+                                                       created_utc=created_utc,score=score)
+                    else:
+                        if parent_data:
+                            sql_insert_has_parent(parent_id=parent_id,comment_id=comment_id,
+                                                author=author,parent=parent_data,subreddit=subreddit,
+                                                created_utc=created_utc,score=score)
+                            paired_rows += 1
+                        else:
+                            # even though it doesn't have the parent passing the parent_id as false to avoid query error.
+                            sql_insert_no_parent(parent_id=parent_id,comment_id=comment_id,
+                                                author=author,subreddit=subreddit,
+                                                created_utc=created_utc,score=score)
+                            
+            if row_counter == 100000:
+                print(f"Row Counter Value {row_counter}, Paired_Rows {paired_rows},Time {datetime.now()}")
+            if row_counter == 500000:
+                print(f"Row Counter Value {row_counter}, Paired_Rows {paired_rows},Time {datetime.now()}")
+            if row_counter == 1000000:
+                print(f"Row Counter Value {row_counter}, Paired_Rows {paired_rows},Time {datetime.now()}")
+                break
--- model/chatbot_database.py
+++ model/chatbot_database.py
@@ -11,20 +11,20 @@ def create_table():
     c.execute(
       """
         CREATE TABLE IF NOT EXISTS chat_reply(
-            parent_id TEXT PRIMARY KEY,
-            comment_id TEXT UNIQUE,
-            parent TEXT,
-            comment TEXT,
-            subreddit TEXT,
-            unix INT,
+            parent_id CHAR PRIMARY KEY,
+            comment_id CHAR UNIQUE,
+            parent CHAR,
+            author CHAR,
+            subreddit CHAR,
+            created_utc INT,
             score INT
-        )
+        );
       """
     )
   
 def find_parent(id):
     try:
-        query = f"SELECT comment FROM chat_reply WHERE comment_id = {id} LIMIT 1"
+        query = f"""SELECT parent FROM chat_reply WHERE parent_id = {id} LIMIT 1"""
         c.execute(query)
         result = c.fetchone()
         if result != None:
@@ -33,3 +33,51 @@ def find_parent(id):
             return False
     except Exception as e:
         return False
+
+def find_existing_score(id):
+    try:
+        query = f"SELECT score FROM chat_reply WHERE parent_id = {id} LIMIT 1"
+        c.execute(query)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return False
+    except Exception as e:
+        return False
+    
+def transaction_bldr(query):
+    global sql_transaction
+    sql_transaction.append(query)
+    if len(sql_transaction) > 1000:
+        c.execute('BEGIN TRANSACTION')
+        for s in sql_transaction:
+            try:
+                c.execute(s)
+                # print(s)
+            except:
+                pass
+        connection.commit()
+        sql_transaction = []
+    
+def sql_insert_replace_comment(parent_id,comment_id,author,parent_data,subreddit,created_utc,score):
+    try:
+        query = f"""UPDATE chat_reply SET parent_id = {parent_id}, comment_id = {comment_id}, parent = {parent_data}, author = {author}, subreddit = {subreddit}, created_utc = {created_utc}, score = {score}"""
+        transaction_bldr(query)
+    except Exception as e:
+        print("query update error",e)
+
+def sql_insert_has_parent(parent_id,comment_id,parent,author,subreddit,created_utc,score):
+    try:
+        query = f'''INSERT INTO chat_reply(parent_id, comment_id, parent, author, subreddit, created_utc, score) VALUES ("{parent_id}","{comment_id}","{parent}","{author}","{subreddit}",{created_utc},{score});'''
+        transaction_bldr(query)
+        print(query)
+    except Exception as e:
+        print("query parent error",e)
+
+def sql_insert_no_parent(parent_id,comment_id,author,subreddit,created_utc,score):
+    try:
+        query = f'''INSERT INTO chat_reply(parent_id, comment_id, author, subreddit, created_utc, score) VALUES ("{parent_id}","{comment_id}","{author}","{subreddit}",{created_utc},{score});'''
+        transaction_bldr(query)
+    except Exception as e:
+        print("query no parent error",e)
