cbf0c247c58f30cd7f0ca59d6a67db9643fbb282
Thanseef <androtsf@gmail.com>
2023-01-27 17:55:52 +0530

    documentation added

--- nlp_nltk.py
+++ nlp_nltk.py
@@ -8,17 +8,25 @@ stemmer = PorterStemmer()
 
 # Tokenizing
 def tokenize(sentence):
+    """
+    words = "hi hello how are you thanseef please contact us !"
+    output = ['hi', 'hello', 'how', 'are', 'you', 'thanseef', 'please', 'contact', 'us', '!']
+    """
     return nltk.word_tokenize(sentence)
   
 # Stemming words
 def stem(word):
+    """
+        tokenized_sentence = ['hi', 'hello', 'How', 'are', 'You', 'Thanseef', 'please', 'contact', 'us', '!']
+        output = ['hi', 'hello', 'how', 'are', 'you', 'thanseef', 'pleas', 'contact', 'us', '!']
+    """
     return stemmer.stem(word=word.lower())
 
 def bag_of_words(tokenized_sentence,all_words):
     """
-    sentence = ['Hai','Hello',"How","are","you"]
-    words = ["Hi","Hai","Hello","Jump","Jet","Are","wait","You"]
-    output = [0,1,1,0,0,1,0,1]
+        sentence = ['Hai','Hello',"How","are","you"]
+        words = ["Hi","Hai","Hello","Jump","Jet","Are","wait","You"]
+        output = [0,1,1,0,0,1,0,1]
     """
     tokenized_sentence = [stem(word) for word in tokenized_sentence]
     bag = np.zeros(len(all_words),dtype=float)
@@ -26,10 +34,3 @@ def bag_of_words(tokenized_sentence,all_words):
         if w.lower() in tokenized_sentence:
             bag[idx] = 1.0
     return bag
-
-
-# sentence = ["Hai","Hello","How","are","you"]
-# words = ["Hi","Hai","Hello","Jump","Jet","Are","wait","You"]
-# output = [0,1,1,0,0,1,0,1]
-# bag = bag_of_words(sentence,words)
-# print(bag)
