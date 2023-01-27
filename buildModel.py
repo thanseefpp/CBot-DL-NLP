4a1165deb253d3e913e455a1143d0afba144171f
Thanseef <androtsf@gmail.com>
2023-01-27 17:49:35 +0530

    bag of words function updated, encoding working

--- buildModel.py
+++ buildModel.py
@@ -1,18 +1,20 @@
+######################################## IMPORTING LIBRARIES ###########################################
 from nlp_nltk import stem,tokenize,bag_of_words
 import json
+import datetime
+import numpy as np
 
+start_time = datetime.datetime.now()
 
+######################################### MODEL IMPORT #################################################
 # importing the Json Model for Training and Testing
-
 with open('ManualDataGenerator/train_data/collection.json', 'r') as f:
     collected_data = json.load(f)
-    
 
-
+######################################### FUNCTIONS HANDLING #################################################
 all_words =  []
 tags = []
 Xy = []
-
 
 for item in collected_data['intents']:
     tag = item['tag']
@@ -21,12 +23,32 @@ for item in collected_data['intents']:
         word = tokenize(pattern)
         all_words.extend(word)
         Xy.append((word,tag))
-        
+
 invalid_words = ['@','!',',','"',':',";",' ','.','#',"_","-",'+'] # you can add more symbols to avoid from the words
-
-# print(f"Before : {all_words} len:{len(all_words)}")
-
 all_words = [stem(word) for word in all_words if word not in invalid_words]
+all_words = sorted(set(all_words))  
+tags = sorted(set(tags))
 
-# print(f"After : {all_words} len:{len(all_words)}")
+######################################### TRAINING MODEL #################################################
+
+X_train = []
+y_train = []
+
+for (patterns,tag) in Xy:
+    bag = bag_of_words(tokenized_sentence=pattern,all_words=all_words)
+    X_train.append(bag)
+    labeled = tags.index(tag)
+    y_train.append(labeled) # to get the tag here manual encoding using the index values to get the actual tag value.
     
+
+# convert to two dimensional Array
+
+X_train = np.asarray(X_train)
+y_train = np.asarray(y_train)
+
+# print(X_train.shape,y_train.shape)
+
+print(f'start time : {start_time}')
+end_time = datetime.datetime.now()
+print(f"end_time : {end_time}")
+print(f"Total Time Taken :{end_time - start_time}")
--- nlp_nltk.py
+++ nlp_nltk.py
@@ -1,4 +1,5 @@
 import nltk
+import numpy as np
 from nltk.stem.porter import PorterStemmer
 # nltk.download('punkt') uncomment this for first time only. if you don't have the package.
 
@@ -14,4 +15,21 @@ def stem(word):
     return stemmer.stem(word=word.lower())
 
 def bag_of_words(tokenized_sentence,all_words):
-    pass
+    """
+    sentence = ['Hai','Hello',"How","are","you"]
+    words = ["Hi","Hai","Hello","Jump","Jet","Are","wait","You"]
+    output = [0,1,1,0,0,1,0,1]
+    """
+    tokenized_sentence = [stem(word) for word in tokenized_sentence]
+    bag = np.zeros(len(all_words),dtype=float)
+    for idx,w in enumerate(all_words):
+        if w.lower() in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
+
+
+# sentence = ["Hai","Hello","How","are","you"]
+# words = ["Hi","Hai","Hello","Jump","Jet","Are","wait","You"]
+# output = [0,1,1,0,0,1,0,1]
+# bag = bag_of_words(sentence,words)
+# print(bag)
