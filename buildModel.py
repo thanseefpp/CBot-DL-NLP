31d0b06383259e2ffed55c7105f4f926e4de26e9
Thanseef <androtsf@gmail.com>
2023-01-28 12:58:50 +0530

    model train done, export done

--- buildModel.py
+++ buildModel.py
@@ -1,18 +1,18 @@
-######################################## IMPORTING LIBRARIES ###########################################
+######################################## IMPORTING LIBRARIES ############################################
 from nlp_nltk import stem,tokenize,bag_of_words
 import json
 import numpy as np
 import torch
-from torch.utils.data import Dataset,DataLoader
+from torch.utils.data import Dataset, DataLoader
 from nn_model import NeuralNet
 import torch.nn as nn
 
-######################################### MODEL IMPORT #################################################
+######################################### MODEL IMPORT ##################################################
 # importing the Json Model for Training and Testing
 with open('ManualDataGenerator/train_data/collection.json', 'r') as f:
     collected_data = json.load(f)
 
-######################################### FUNCTIONS HANDLING #################################################
+######################################### FUNCTIONS HANDLING ############################################
 all_words =  []
 tags = []
 Xy = []
@@ -25,7 +25,7 @@ for item in collected_data['intents']:
         all_words.extend(word)
         Xy.append((word,tag))
 
-invalid_words = ['@','!',',','"',':',";",' ','.','#',"_","-",'+'] # you can add more symbols to avoid from the words
+invalid_words = ['@','!',',','"',':',";",' ','.','#',"_","-",'+','?'] # you can add more symbols to avoid from the words
 all_words = [stem(word) for word in all_words if word not in invalid_words]
 all_words = sorted(set(all_words))  
 tags = sorted(set(tags))
@@ -36,7 +36,7 @@ X_train = []
 y_train = []
 
 for (patterns,tag) in Xy:
-    bag = bag_of_words(tokenized_sentence=pattern,words=all_words)
+    bag = bag_of_words(tokenized_sentence=patterns,words=all_words)
     X_train.append(bag)
     labeled = tags.index(tag)
     y_train.append(labeled) # to get the tag here manual encoding using the index values to get the actual tag value.
@@ -44,8 +44,8 @@ for (patterns,tag) in Xy:
 
 # convert to two dimensional Array
 
-X_train = np.asarray(X_train)
-y_train = np.asarray(y_train)
+X_train = np.array(X_train)
+y_train = np.array(y_train)
 
 
 class ChatDataset(Dataset):
@@ -75,12 +75,12 @@ train_loader    = DataLoader(dataset=dataset,batch_size=batch_size,shuffle=True,
 
 # checking our pc have cuda support(NVIDIA Graphics Feature) else we will use cpu for building Neural Network.
 device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model   = NeuralNet(input_size,hidden_size,output_size).to(device=device)
+model   = NeuralNet(input_size, hidden_size, output_size).to(device)
 
 
 # Loss and Optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 for epoch in range(number_epochs):
     for (words,labels) in train_loader:
@@ -101,3 +101,19 @@ for epoch in range(number_epochs):
         
         
 print(f"Final Loss {loss.item():.4f}")
+
+######################################### EXPORT TRAINED MODEL ##############################################
+
+trained_model = {
+    "model_state"   : model.state_dict(),
+    "input_size"    : input_size,
+    "output_size"   : output_size,
+    "hidden_size"   : hidden_size,
+    "all_words"     : all_words,
+    "tags"          : tags
+}
+# Save Trained Model as a file
+EXPORT_FILE = "model/trained_model.pth"
+torch.save(trained_model,EXPORT_FILE)
+
+print("Model Pickling Done")
